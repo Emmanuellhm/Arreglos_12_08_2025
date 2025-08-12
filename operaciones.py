7217e2e760c64043be185e6752084fade00166e3
Emmanuel Hincapie <144484860+Emmanuellhm@users.noreply.github.com>
2025-08-12 11:30:31 -0500

    mi segundo archivo

---
+++ operaciones.py
@@ -0,0 +1,18 @@
+import numpy as np
+vector1 = np.array([1, 2, 3])
+vector2 = np.array([4, 5, 6])
+#Suma de vectores
+suma = vector1 + vector2 
+print("suma de vectores: ", suma)
+#Multiplicacion de vectores
+producto = vector1 * vector2 
+print("producto de vectores:", producto)
+#Division de vectores
+division = vector1 / vector2 
+print("division de vectores:", division)
+#Resta de vectores 
+resta = vector1 - vector2
+print("resta de vectores:", resta)
+#Potencia de vectores
+potencia = vector1 **2
+print("/x1b[1;33m"+"potencia de vectores:", potencia)
